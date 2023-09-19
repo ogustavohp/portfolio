5da6c067c239e19af80dbf771f7daa915b5923de
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-09-19 19:01:39 -0300

    att `WhatIDo` section

--- src/components/WhatIDo/index.tsx
+++ src/components/WhatIDo/index.tsx
@@ -1,27 +1,21 @@
 import React from 'react'
 import Typography from '../Typography'
 import WhatIDoCard from './WhatIDoCard'
+import db from '@/db/db.json'
 
 export default function WhatIDo() {
   return (
     <section className="flex flex-col items-center gap-3">
       <Typography Tag="h2" apparentTag="h2" ornament botBorder>
-        O Que eu faço?
+        {db.WhatIDo.title}
       </Typography>
       {/* Card oq eu faço */}
       <div className="flex flex-wrap justify-center gap-7">
-        <WhatIDoCard title="Front-end">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-          beatae optio ullam excepturi voluptatum sapiente accusamus architecto,
-          sed porro aliquid veritatis, nihil possimus debitis aperiam animi quia
-          deleniti quod hic!
-        </WhatIDoCard>
-        <WhatIDoCard title="Back-end">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-          dignissimos magnam, molestias error reprehenderit molestiae natus,
-          incidunt minus repellat voluptatem recusandae praesentium, tempora
-          voluptatum aspernatur veritatis non nam magni culpa.
-        </WhatIDoCard>
+        {db.WhatIDo.WhatIDoList.map((e) => (
+          <WhatIDoCard key={e.id} title={e.title}>
+            {e.description}
+          </WhatIDoCard>
+        ))}
       </div>
     </section>
   )
