ba8c875cccff373558d0c2f15b3b78cb1056ba41
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-09-19 18:52:17 -0300

    att `Differences` section

--- src/components/Differences/DifferentialCard.tsx
+++ src/components/Differences/DifferentialCard.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import Typography from '../Typography'
 import Image from 'next/image'
+import iconTag from '@/assets/tag.svg'
 
 interface IDifferentialCard {
   title: string
@@ -13,21 +14,31 @@ export default function DifferentialCard({
   icon,
   children,
 }: IDifferentialCard) {
-  return (
-    <div className="flex w-60 flex-col items-center gap-3 rounded-xl border border-iconcolor-500 bg-gradient-to-b from-slate-900 to-bgcolor-500 px-2 pb-3 pt-2">
-      <Image alt="Icone </>" src={icon} />
-      <div className="text-center">
-        <Typography
-          Tag="h3"
-          apparentTag="cardDifferencesTitle"
-          style={'text-primary-500'}
-        >
-          {title}
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          {children}
-        </Typography>
+  if (icon === 'iconTag') {
+    const icons = {
+      iconTag,
+    }
+
+    return (
+      <div className="flex w-60 flex-col items-center gap-3 rounded-xl border border-iconcolor-500 bg-gradient-to-b from-slate-900 to-bgcolor-500 px-2 pb-3 pt-2">
+        <Image alt="Icone </>" src={icons[icon]} />
+        <div className="text-center">
+          <Typography
+            Tag="h3"
+            apparentTag="cardDifferencesTitle"
+            style={'text-primary-500'}
+          >
+            {title}
+          </Typography>
+          <Typography Tag="p" apparentTag="p">
+            {children}
+          </Typography>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    throw new Error(
+      `Ícone não aceito em DifferentialCard @/components/differences/DifferentialCard.tsx o ícone veio de @/db/db.json differences.differencesList>icon`,
+    )
+  }
 }
--- src/components/Differences/index.tsx
+++ src/components/Differences/index.tsx
@@ -1,39 +1,21 @@
 import React from 'react'
 import Typography from '../Typography'
 import DifferentialCard from './DifferentialCard'
-import iconTag from '@/assets/tag.svg'
+import db from '@/db/db.json'
 
 export default function Differences() {
   return (
-    <section className="flex flex-col items-center gap-3">
+    <section className="flex flex-col items-center justify-between gap-3">
       <Typography Tag="h2" apparentTag="h1" ornament botBorder>
-        Diferenciais
+        {db.differences.title}
       </Typography>
 
       <div className="flex flex-wrap justify-center gap-3 ">
-        <DifferentialCard icon={iconTag} title="Clean Code">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae odio
-          laudantium tenetur quas suscipit sint ut consequatur neque officia id
-          placeat nesciunt eos in, aliquam nam nulla blanditiis quod minus?
-        </DifferentialCard>
-
-        <DifferentialCard icon={iconTag} title="Clean Code">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae odio
-          laudantium tenetur quas suscipit sint ut consequatur neque officia id
-          placeat nesciunt eos in, aliquam nam nulla blanditiis quod minus?
-        </DifferentialCard>
-
-        <DifferentialCard icon={iconTag} title="Clean Code">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae odio
-          laudantium tenetur quas suscipit sint ut consequatur neque officia id
-          placeat nesciunt eos in, aliquam nam nulla blanditiis quod minus?
-        </DifferentialCard>
-
-        <DifferentialCard icon={iconTag} title="Clean Code">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae odio
-          laudantium tenetur quas suscipit sint ut consequatur neque officia id
-          placeat nesciunt eos in, aliquam nam nulla blanditiis quod minus?
-        </DifferentialCard>
+        {db.differences.differencesList.map((e) => (
+          <DifferentialCard key={e.id} icon={e.icon} title={e.title}>
+            {e.description}
+          </DifferentialCard>
+        ))}
       </div>
     </section>
   )
