958efa6e0c2b12c0868f7fb4b9d32c85717e3f84
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-09-20 02:38:04 -0300

    att `menu` section

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,6 +5,9 @@ import Image from 'next/image'
 import iconMenuh from '@/assets/menuh.svg'
 import Link from 'next/link'
 import Button from '../Button'
+import db from '@/db/db.json'
+
+const menu = db.menu
 
 export default function Menu() {
   const [visible, setVisible] = useState<boolean>(false)
@@ -32,71 +35,36 @@ export default function Menu() {
         >
           <Link href={'/aboutme'} className="pt-4 md:pt-0" onClick={change}>
             <Typography Tag="span" apparentTag="cardDifferencesTitle">
-              Sobre mim
+              {menu.aboutMe}
             </Typography>
           </Link>
           <Link href={'/formations'} onClick={change}>
             <Typography Tag="span" apparentTag="cardDifferencesTitle">
-              Formações
+              {menu.formations}
             </Typography>
           </Link>
           <Link href={'/skills'} onClick={change}>
             <Typography Tag="span" apparentTag="cardDifferencesTitle">
-              Habilidades
+              {menu.skills}
             </Typography>
           </Link>
           <Link href={'/projects'} onClick={change}>
             <Typography Tag="span" apparentTag="cardDifferencesTitle">
-              Projetos
+              {menu.projects}
             </Typography>
           </Link>
           <Link href={'#'} onClick={change}>
             <Button large variant="FillSecondary">
-              Currículo
+              {menu.cv}
             </Button>
           </Link>
           <Link href={'#contact'} onClick={change}>
             <Button variant="primary" large>
-              Contato
+              {menu.contact}
             </Button>
           </Link>
         </div>
       </div>
-
-      {/* <div
-        className={`absolute flex w-full flex-col items-start gap-5 bg-slate-900/95 pb-4 pl-16 transition-all ${
-          visible ? 'left-0' : '-left-full'
-        } md:static md:flex-row md:items-center md:justify-center md:bg-transparent md:pl-0 lg:hidden`}
-      >
-        <Link href={'/aboutme'} className="pt-4 md:pt-0" onClick={change}>
-          <Typography Tag="span" apparentTag="cardDifferencesTitle">
-            Sobre mim
-          </Typography>
-        </Link>
-        <Link href={'/formations'} onClick={change}>
-          <Typography Tag="span" apparentTag="cardDifferencesTitle">
-            Formações
-          </Typography>
-        </Link>
-        <Link href={'/skills'} onClick={change}>
-          <Typography Tag="span" apparentTag="cardDifferencesTitle">
-            Habilidades
-          </Typography>
-        </Link>
-        <Link href={'/projects'} onClick={change}>
-          <Typography Tag="span" apparentTag="cardDifferencesTitle">
-            Projetos
-          </Typography>
-        </Link>
-        <Link href={'#'} onClick={change}>
-          <Button large>Currículo</Button>
-        </Link>
-        <Link href={'#'} onClick={change}>
-          <Button variant="primary" large>
-            Contato
-          </Button>
-        </Link>
-      </div> */}
     </header>
   )
 }
