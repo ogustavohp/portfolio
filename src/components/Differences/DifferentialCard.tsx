d10b54721293a8e449151866b7a9d8cffc7c80c7
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-10-01 03:23:55 -0300

    cards `differential` and `db.json differences`

--- src/components/Differences/DifferentialCard.tsx
+++ src/components/Differences/DifferentialCard.tsx
@@ -21,7 +21,7 @@ export default function DifferentialCard({
 
     return (
       <div className="flex w-60 flex-col items-center gap-3 rounded-xl border border-iconcolor-500 bg-gradient-to-b from-slate-900 to-bgcolor-500 px-2 pb-3 pt-2">
-        <Image alt="Icone </>" src={icons[icon]} />
+        <Image alt="Ícone </>" src={icons[icon]} />
         <div className="text-center">
           <Typography
             Tag="h3"
@@ -30,9 +30,11 @@ export default function DifferentialCard({
           >
             {title}
           </Typography>
-          <Typography Tag="p" apparentTag="p">
-            {children}
-          </Typography>
+          <span className="hyphens-auto">
+            <Typography Tag="p" apparentTag="p">
+              {children}
+            </Typography>
+          </span>
         </div>
       </div>
     )
