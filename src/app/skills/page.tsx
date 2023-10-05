cb91088fcd3a2fc6e3d2a68c9d9d8e7c9b13b484
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-10-05 16:46:10 -0300

    add title in skills page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import SkillsTechnologies from '@/components/Skills/SkillsTechnologies'
 import SkillInputSearch from '@/components/Skills/SkillInputSearch'
+import Typography from '@/components/Typography'
+import db from '@/db/db.json'
+
+const skillsAndTechnologiesDb = db.skillsAndTechnologies.title1
 
 export default function Skills() {
   return (
     <main className="mx-5 mt-8 flex flex-col items-center gap-7">
+      <Typography Tag="h1" apparentTag="h1" ornament botBorder>
+        {skillsAndTechnologiesDb}
+      </Typography>
       {/* Input de busca */}
-
       <SkillInputSearch />
 
       {/* technology */}
