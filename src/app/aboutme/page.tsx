7b8c5ad70699ff72b4df9dd7364a72ce62d65148
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-10-10 03:38:30 -0300

    att db.json

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -1,6 +1,9 @@
 import AboutMeCardPage from '@/components/AboutMe/AboutMeCardPage'
 import Typography from '@/components/Typography'
 import React from 'react'
+import db from '@/db/db.json'
+
+const aboutMe = db.aboutMe.pageText
 
 export default function Page() {
   return (
@@ -9,147 +12,69 @@ export default function Page() {
         <AboutMeCardPage />
       </div>
       <div className="flex max-w-lg flex-col gap-4">
-        <Typography Tag="h1" apparentTag="h1" ornament>
-          Olá! Eu sou o Gustavo
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet,
-          libero eos repellendus aperiam porro eligendi! Autem id delectus
-          consectetur animi maxime ipsum perferendis voluptatem harum, commodi,
-          iste nulla earum est?Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Deserunt adipisci suscipit fugit at totam culpa.
-          Adipisci sit, veniam aut aperiam alias eaque assumenda quaerat nemo
-          perferendis, nisi incidunt excepturi fuga.
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Minha História com a Programação
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-          cupiditate iure ducimus odit delectus aliquid nesciunt alias maiores
-          laborum, dolor perspiciatis aut dolores voluptates dolore animi
-          facilis tenetur quod possimus? Adipisci dolore, quas soluta molestiae
-          amet excepturi rerum id quidem, iste itaque est maiores laudantium
-          beatae, neque magni laborum! Eius voluptatum repudiandae molestiae
-          assumenda labore ratione iste commodi laborum rem.
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Lorem ipsum
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-          aperiam molestiae officiis vitae alias veritatis, velit incidunt
-          minima omnis fugiat aliquid voluptatem necessitatibus consequatur
-          blanditiis dolores amet quisquam perspiciatis facere? Ex numquam
-          laboriosam aspernatur corporis sunt odio, esse, assumenda in illo
-          recusandae tenetur eligendi vero, quo optio reprehenderit nulla
-          voluptate minus quidem. Excepturi nulla, non ipsum officiis dolorum
-          cupiditate dolor!
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Lorem ipsum
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-          aperiam molestiae officiis vitae alias veritatis, velit incidunt
-          minima omnis fugiat aliquid voluptatem necessitatibus consequatur
-          blanditiis dolores amet quisquam perspiciatis facere? Ex numquam
-          laboriosam aspernatur corporis sunt odio, esse, assumenda in illo
-          recusandae tenetur eligendi vero, quo optio reprehenderit nulla
-          voluptate minus quidem. Excepturi nulla, non ipsum officiis dolorum
-          cupiditate dolor!
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Lorem ipsum
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-          aperiam molestiae officiis vitae alias veritatis, velit incidunt
-          minima omnis fugiat aliquid voluptatem necessitatibus consequatur
-          blanditiis dolores amet quisquam perspiciatis facere? Ex numquam
-          laboriosam aspernatur corporis sunt odio, esse, assumenda in illo
-          recusandae tenetur eligendi vero, quo optio reprehenderit nulla
-          voluptate minus quidem. Excepturi nulla, non ipsum officiis dolorum
-          cupiditate dolor!
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Lorem ipsum
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-          aperiam molestiae officiis vitae alias veritatis, velit incidunt
-          minima omnis fugiat aliquid voluptatem necessitatibus consequatur
-          blanditiis dolores amet quisquam perspiciatis facere? Ex numquam
-          laboriosam aspernatur corporis sunt odio, esse, assumenda in illo
-          recusandae tenetur eligendi vero, quo optio reprehenderit nulla
-          voluptate minus quidem. Excepturi nulla, non ipsum officiis dolorum
-          cupiditate dolor!
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Lorem ipsum
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-          aperiam molestiae officiis vitae alias veritatis, velit incidunt
-          minima omnis fugiat aliquid voluptatem necessitatibus consequatur
-          blanditiis dolores amet quisquam perspiciatis facere? Ex numquam
-          laboriosam aspernatur corporis sunt odio, esse, assumenda in illo
-          recusandae tenetur eligendi vero, quo optio reprehenderit nulla
-          voluptate minus quidem. Excepturi nulla, non ipsum officiis dolorum
-          cupiditate dolor!
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Lorem ipsum
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-          aperiam molestiae officiis vitae alias veritatis, velit incidunt
-          minima omnis fugiat aliquid voluptatem necessitatibus consequatur
-          blanditiis dolores amet quisquam perspiciatis facere? Ex numquam
-          laboriosam aspernatur corporis sunt odio, esse, assumenda in illo
-          recusandae tenetur eligendi vero, quo optio reprehenderit nulla
-          voluptate minus quidem. Excepturi nulla, non ipsum officiis dolorum
-          cupiditate dolor!
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Lorem ipsum
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-          aperiam molestiae officiis vitae alias veritatis, velit incidunt
-          minima omnis fugiat aliquid voluptatem necessitatibus consequatur
-          blanditiis dolores amet quisquam perspiciatis facere? Ex numquam
-          laboriosam aspernatur corporis sunt odio, esse, assumenda in illo
-          recusandae tenetur eligendi vero, quo optio reprehenderit nulla
-          voluptate minus quidem. Excepturi nulla, non ipsum officiis dolorum
-          cupiditate dolor!
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Lorem ipsum
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-          aperiam molestiae officiis vitae alias veritatis, velit incidunt
-          minima omnis fugiat aliquid voluptatem necessitatibus consequatur
-          blanditiis dolores amet quisquam perspiciatis facere? Ex numquam
-          laboriosam aspernatur corporis sunt odio, esse, assumenda in illo
-          recusandae tenetur eligendi vero, quo optio reprehenderit nulla
-          voluptate minus quidem. Excepturi nulla, non ipsum officiis dolorum
-          cupiditate dolor!
-        </Typography>
-        <Typography Tag="h2" apparentTag="h1" ornament>
-          Lorem ipsum
-        </Typography>
-        <Typography Tag="p" apparentTag="p">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-          aperiam molestiae officiis vitae alias veritatis, velit incidunt
-          minima omnis fugiat aliquid voluptatem necessitatibus consequatur
-          blanditiis dolores amet quisquam perspiciatis facere? Ex numquam
-          laboriosam aspernatur corporis sunt odio, esse, assumenda in illo
-          recusandae tenetur eligendi vero, quo optio reprehenderit nulla
-          voluptate minus quidem. Excepturi nulla, non ipsum officiis dolorum
-          cupiditate dolor!
-        </Typography>
+        {aboutMe.h1.map((e) => (
+          <div key={e.id} className="space-y-4">
+            <Typography Tag="h1" apparentTag="h1" ornament>
+              {e.title}
+            </Typography>
+            {e.text.map((e) => (
+              <Typography key={e} Tag="p" apparentTag="p">
+                {e}
+              </Typography>
+            ))}
+          </div>
+        ))}
+        {aboutMe.h2.map((e) => (
+          <div key={e.id} className="space-y-4">
+            <Typography Tag="h2" apparentTag="h1" ornament>
+              {e.title}
+            </Typography>
+            {e.text.map((e) => (
+              <Typography key={e} Tag="p" apparentTag="p">
+                {e}
+              </Typography>
+            ))}
+          </div>
+        ))}
+        {aboutMe.h3.map((e) => (
+          <div key={e.id} className="space-y-4">
+            <Typography Tag="h3" apparentTag="h1" ornament>
+              {e.title}
+            </Typography>
+            {e.text.map((e) => (
+              <Typography key={e} Tag="p" apparentTag="p">
+                {e}
+              </Typography>
+            ))}
+            {e.skills.map((e) => (
+              <div key={e.title} className="space-y-1">
+                <Typography Tag="span" apparentTag="p" botBorder>
+                  {e.title}
+                </Typography>
+                {e.skill.map((e) => (
+                  <p key={e.name} className="text-white">
+                    <span className="font-bold text-primary-500">
+                      {e.name}:{' '}
+                    </span>
+                    {e.description}
+                  </p>
+                ))}
+              </div>
+            ))}
+          </div>
+        ))}
+        {aboutMe.h4.map((e) => (
+          <div key={e.id} className="space-y-4">
+            <Typography Tag="h4" apparentTag="h1" ornament>
+              {e.title}
+            </Typography>
+            {e.text.map((e) => (
+              <Typography key={e} Tag="p" apparentTag="p">
+                {e}
+              </Typography>
+            ))}
+          </div>
+        ))}
       </div>
     </main>
   )
