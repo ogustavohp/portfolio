af806335a1f8177163849f474efae17082783b8f
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-10-12 20:59:45 -0300

    add image me

--- src/components/AboutMe/ProfileImage.tsx
+++ src/components/AboutMe/ProfileImage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Camera } from 'lucide-react'
-import colors from '@/db/colors.json'
+
+import Image from 'next/image'
+import me from '@/assets/me.png'
 
 export default function ProfileImage() {
   return (
     <div className="mb-2 flex h-52 w-52 items-center justify-center rounded-full border border-primary-500 bg-iconcolor-700 lg:h-72 lg:w-72">
-      <Camera color={colors.othersIconColors[1]} size={100} strokeWidth={1.5} />
+      <Image alt="Foto minha" src={me} className="rounded-full" />
     </div>
   )
 }
