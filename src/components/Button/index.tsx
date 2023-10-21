ae674c8f945600d4839351b156c105b7b6964f2d
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-10-21 16:39:26 -0300

    change button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,17 @@
 import React, { ReactNode } from 'react'
-import Typography from '../Typography'
 
 interface IButton {
   children: ReactNode
   variant?: 'FillPrimary' | 'FillSecondary' | 'primary' | 'secondary'
   large?: boolean
+  hover?: boolean
 }
 
 export default function Button({
   children,
   variant = 'FillPrimary',
   large = false,
+  hover = false,
 }: IButton) {
   const colorVariants = {
     FillPrimary: 'bg-secondary-500',
@@ -23,19 +24,20 @@ export default function Button({
     <div
       className={`flex gap-2 rounded-full ${colorVariants[variant]} ${
         large ? 'text-xl' : ''
-      } px-4 py-1`}
+      } ${
+        hover && 'transition-all hover:bg-primary-500'
+      } px-4 py-1 leading-relaxed
+      ${
+        variant === 'FillPrimary' || variant === 'FillSecondary'
+          ? 'text-bgcolor-900'
+          : ''
+      } ${
+        (hover && variant === 'primary') || (hover && variant === 'secondary')
+          ? ' text-white transition-all hover:text-bgcolor-900'
+          : ''
+      }`}
     >
-      <Typography
-        Tag="p"
-        apparentTag="p"
-        style={
-          variant === 'FillPrimary' || variant === 'FillSecondary'
-            ? 'text-bgcolor-900'
-            : ''
-        }
-      >
-        {children}
-      </Typography>
+      {children}
     </div>
   )
 }
